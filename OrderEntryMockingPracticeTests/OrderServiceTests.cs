1e09119f9228a52fe02efbad7474c1435d3e0257
Michael Huang <mihuan@microsoft.com>
2018-08-02 14:27:14 -0700

    Added test to see if all products are in stock

--- OrderEntryMockingPracticeTests/OrderServiceTests.cs
+++ OrderEntryMockingPracticeTests/OrderServiceTests.cs
@@ -1,6 +1,77 @@
-﻿namespace OrderEntryMockingPracticeTests
+﻿using Xunit;
+using Rhino.Mocks;
+using OrderEntryMockingPractice.Services;
+using OrderEntryMockingPractice.Models;
+
+namespace OrderEntryMockingPracticeTests
 {
     public class OrderServiceTests
     {
+        [Fact]
+        public void OrderItemsAreUnique()
+        {
+            // Arrange
+            //var stubOrderProvider = MockRepository.GenerateStub<>
+
+            // Act
+
+            // Assert
+
+        }
+
+        [Fact]
+        public void AllProductsAreInStock()
+        {
+            // Arrange
+            var stubProductProvider = MockRepository.GenerateStub<IProductRepository>();
+            var product = new Product();
+
+            // Act
+            bool isInStock = stubProductProvider.IsInStock(product.Sku);
+
+            // Assert
+            Assert.True(isInStock);
+        }
+
+        [Fact]
+        public void OrderIsValid()
+        {
+            // Arrange
+
+            // Act
+
+            // Assert
+        }
+
+        [Fact]
+        public void CustomerInfoCanBeRetrieved()
+        {
+            // Arrange
+
+            // Act
+
+            // Assert
+        }
+
+        [Fact]
+        public void TaxesCanBeRetrieved()
+        {
+            // Arrange
+
+            // Act
+
+            // Assert
+        }
+
+        [Fact]
+        public void ProductRepositoryCanDetermineStock()
+        {
+            // Arrange
+
+            // Act
+
+            // Assert
+        }
+    
     }
 }
