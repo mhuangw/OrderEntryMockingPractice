06e0f4a967c73e1602d321c0b5cbd2694ea8194c
Michael Huang <mihuan@microsoft.com>
2018-08-13 10:00:08 -0700

    Replaced old code with Chris's starter tests

--- OrderEntryMockingPracticeTests/OrderServiceTests.cs
+++ OrderEntryMockingPracticeTests/OrderServiceTests.cs
@@ -6,7 +6,7 @@ using System.Collections.Generic;
 
 namespace OrderEntryMockingPracticeTests
 {
-    public class OrderServiceTests
+    public class CMOrderServiceTests
     {
         private ICustomerRepository _customerRepository;
         private IEmailService _emailService;
@@ -14,112 +14,51 @@ namespace OrderEntryMockingPracticeTests
         private IProductRepository _productRepository;
         private ITaxRateService _taxRateService;
 
-        public OrderServiceTests()
+        public CMOrderServiceTests()
         {
             _customerRepository = MockRepository.GenerateMock<ICustomerRepository>();
             _emailService = MockRepository.GenerateMock<IEmailService>();
             _orderFulfillmentService = MockRepository.GenerateMock<IOrderFulfillmentService>();
             _productRepository = MockRepository.GenerateMock<IProductRepository>();
             _taxRateService = MockRepository.GenerateMock<ITaxRateService>();
+
+            this.OrderService = new OrderService();
+        }
+        public OrderService OrderService { get; set; }
+
+        [Fact]
+        public void ReturnsOrderSummary()
+        {
+            // Given
+            var order = new Order();
+
+            // When
+            var result = this.OrderService.PlaceOrder(order);
+
+            // Then
+            result.ShouldNotBeNull();
         }
 
-        public bool IsUnique(Order order)
+        [Fact]
+        public void SubmitOrderToFulfillment()
         {
-            HashSet<string> skusSeen = new HashSet<string>();
-
-            foreach (OrderItem orderItem in order.OrderItems)
-            {
-                if (skusSeen.Contains(orderItem.Product.Sku))
+            // Given
+            var order = new Order();
+            _orderFulfillmentService
+                .Stub(o => o.Fulfill(order))
+                .Return(new OrderConfirmation()
                 {
-                    return false;
-                }
-                skusSeen.Add(orderItem.Product.Sku);
-            }
-            return true;
+                    OrderNumber = "1001"
+                });
+
+            // When
+            var result = this.OrderService.PlaceOrder(order);
+
+            // Then
+            _orderFulfillmentService.AssertWasCalled(o => o.Fulfill(order));
+            result.OrderNumber.ShouldBe("1001");
         }
 
-        [Fact]
-        public void OrderItemsAreUnique()
-        {
-            // Arrange
-            var orderStub = MockRepository.GenerateStub<Order>();
-
-            // Act
-            bool orderItemsAreUnique = IsUnique(orderStub);
-
-            // Assert
-            Assert.True(orderItemsAreUnique);
-        }
-
-        [Fact]
-        public void AllProductsAreInStock()
-        {
-            // Arrange
-            var productRepositoryStub = MockRepository.GenerateStub<IProductRepository>();
-            var productStub = MockRepository.GenerateStub<Product>();
-
-            // Act
-            bool isInStock = productRepositoryStub.IsInStock(productStub.Sku);
-
-            // Assert
-            Assert.True(isInStock);
-        }
-
-        public void OrderIsValid()
-        {
-            bool isValid = true;
-            if (isValid)
-            {
-                Order orderStub = MockRepository.GenerateStub<Order>();
-                _orderFulfillmentService.Fulfill(orderStub);
-                _emailService.SendOrderConfirmationEmail(customerId, orderId);
-            } else
-            {
-                throw new Exception();
-            }
-        }
-
-        [Fact]
-        public void CustomerInfoCanBeRetrieved()
-        {
-            // Arrange
-            var customerRepositoryStub = MockRepository.GenerateStub<ICustomerRepository>();
-            var customerStub = MockRepository.GenerateStub<Customer>();
-
-            // Act
-            var customer = customerRepositoryStub.Get(customerStub.CustomerId.Value);
-
-            // Assert
-            Assert.NotNull(customer);
-        }
-
-        [Fact]
-        public void TaxesCanBeRetrieved()
-        {
-            // Arrange
-            var taxRateServiceStub = MockRepository.GenerateStub<ITaxRateService>();
-            var customerStub = MockRepository.GenerateStub<Customer>();
-
-            // Act
-            var taxesStub = taxRateServiceStub.GetTaxEntries(customerStub.PostalCode, customerStub.Country);
-
-            // Assert
-            Assert.NotEmpty(taxesStub);
-        }
-
-        [Fact]
-        public void ProductRepositoryCanDetermineStock()
-        {
-            // Arrange
-            var productRepositoryStub = MockRepository.GenerateStub<IProductRepository>();
-            var productStub = MockRepository.GenerateStub<Product>();
-
-            // Act
-            var isInStock = productRepositoryStub.IsInStock(productStub.Sku);
-
-            // Assert
-            Assert.True(isInStock);
-        }
-    
     }
 }
+
